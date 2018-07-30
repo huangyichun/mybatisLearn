3ab3d2444114530896a41827a2e93307d4a3a052
huangyichun <huangyichun@jd.com>
2018-07-30 13:49:20 +0800

    添加findById和findAll方法

--- src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package tk.mybatis.simple.mapper;
 
+import tk.mybatis.simple.model.SysUser;
+
+import java.util.List;
+
 public interface UserMapper {
+
+    SysUser selectById(Long id);
+
+    List<SysUser> selectAll();
 }
