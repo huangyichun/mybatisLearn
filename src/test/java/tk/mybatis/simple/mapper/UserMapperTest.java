4d1f51cf516aca3bd7965564c659e80bf70625c1
huangyichun <huangyichun@jd.com>
2018-07-30 14:02:46 +0800

    添加findById和findByAll测试方法

---
+++ src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -0,0 +1,37 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import tk.mybatis.simple.model.SysUser;
+
+import java.util.List;
+
+public class UserMapperTest extends BaseMapperTest {
+
+    @Test
+    public void testSelectById(){
+        SqlSession sqlSession = getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = userMapper.selectById(1l);
+            Assert.assertNotNull(user);
+            Assert.assertEquals("admin", user.getUserName());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testSelectAll(){
+        SqlSession sqlSession = getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<SysUser> users = userMapper.selectAll();
+            Assert.assertNotNull(users);
+            Assert.assertTrue(users.size() > 1);
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
