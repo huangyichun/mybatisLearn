3eaca40be16e9581cd47958bb1d05e3a18c38d54
huangyichun <huangyichun@jd.com>
2018-07-30 14:34:06 +0800

    添加selectRolesByUserId对应测试方法

--- src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
+++ src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -3,6 +3,7 @@ package tk.mybatis.simple.mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
 import org.junit.Test;
+import tk.mybatis.simple.model.SysRole;
 import tk.mybatis.simple.model.SysUser;
 
 import java.util.List;
@@ -30,6 +31,19 @@ public class UserMapperTest extends BaseMapperTest {
             List<SysUser> users = userMapper.selectAll();
             Assert.assertNotNull(users);
             Assert.assertTrue(users.size() > 1);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testSelectRolesByUserId(){
+        SqlSession sqlSession = getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<SysRole> roles = userMapper.selectRolesByUserId(1l);
+            Assert.assertNotNull(roles);
+            Assert.assertTrue(roles.size()>0);
         } finally {
             sqlSession.close();
         }
