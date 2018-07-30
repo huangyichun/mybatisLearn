e465888d5dcee3c6754f327d219191120bccc487
huangyichun <huangyichun@jd.com>
2018-07-30 13:54:02 +0800

    继承BaseMapperTest

--- src/test/java/tk/mybatis/simple/mapper/CountryMapperTest.java
+++ src/test/java/tk/mybatis/simple/mapper/CountryMapperTest.java
@@ -8,29 +8,15 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import tk.mybatis.simple.model.Country;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 
-public class CountryMapperTest {
-    private static SqlSessionFactory sqlSessionFactory;
-
-    @BeforeClass
-    public static void setUp(){
-        try {
-            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class CountryMapperTest extends BaseMapperTest{
 
     @Test
     public void testSelectAll(){
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = getSqlSession();
         try {
-            List<Country> countryList = sqlSession.selectList("selectAll");
+            List<Country> countryList = sqlSession.selectList("tk.mybatis.simple.mapper.CountryMapper.selectAll");
             printCountryList(countryList);
         } finally {
             sqlSession.close();
